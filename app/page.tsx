3d855d7c10a1150a3aa39a82464d1214c09b6d51
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-06-07 22:29:06 +0200

    feat: container done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import FlagContainer from '@/containers/Flag.container'
 
 export default function Home() {
   return (
-    <main className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
+    <main>
       <FlagContainer />
     </main>
   )
--- containers/Flag.container.tsx
+++ containers/Flag.container.tsx
@@ -9,32 +9,32 @@ export default function FlagContainer() {
   const { columns, rows, loading } = useFlag();
 
   return (
-    <>
+    <main className="w-[100vw] h-[100vh] flex flex-col items-center justify-center bg-black">
       {loading && <Loader />}
-        <main className="w-[90vw] h-[70vw] lg:w-[1024px] lg:h-[768px] flex flex-col items-center justify-center py-[10px] bg-black">
-          {rows.map((row) => {
-            return (
-              <div key={`row-${row}`} className={`relative flex flex-row w-full flex-1`}>
-                {columns.map((column) => {
-                  return (
-                    <div
-                      key={`row-${row}-column-${column}`}
-                      className={`h-full relative flex-1 ${style[`row-${row}`]} ${style[`column-${column}`]} ${style.column}`}
-                      style={{
-                        animationName: style.oscilate,
-                        animationDuration: '1.5s',
-                        animationTimingFunction: 'ease-in-out',
-                        animationIterationCount: 'infinite',
-                        animationDelay: `${column.length <= 1 ? `0.${column}s` : `${column[0]}.${column[1]}s`}`,
-                      }}
-                    >
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          })}
-        </main>
-    </>
+      <div className="w-[90vw] h-[70vw] lg:w-[1024px] lg:h-[768px] flex flex-col items-center justify-center py-[10px] bg-black">
+        {rows.map((row) => {
+          return (
+            <div key={`row-${row}`} className={`relative flex flex-row w-full flex-1`}>
+              {columns.map((column) => {
+                return (
+                  <div
+                    key={`row-${row}-column-${column}`}
+                    className={`h-full relative flex-1 ${style[`row-${row}`]} ${style[`column-${column}`]} ${style.column}`}
+                    style={{
+                      animationName: style.oscilate,
+                      animationDuration: '1.5s',
+                      animationTimingFunction: 'ease-in-out',
+                      animationIterationCount: 'infinite',
+                      animationDelay: `${column.length <= 1 ? `0.${column}s` : `${column[0]}.${column[1]}s`}`,
+                    }}
+                  >
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </main>
   );
 }
