e126c1dd3f5f2a9489316a9014c3a8d3963d6934
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-06-07 20:43:18 +0200

    fix: actions v5

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: false,
   swcMinify: true,
   basePath: isProduction ? '/waving_flag' : '',
