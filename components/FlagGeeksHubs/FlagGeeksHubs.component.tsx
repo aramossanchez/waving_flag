910a915c64a2156903693d1bc7f4240d5068fa61
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-06-08 23:29:50 +0200

    fix: bandera geekshubs con colores correctos

--- components/FlagGeeksHubs/FlagGeeksHubs.component.tsx
+++ components/FlagGeeksHubs/FlagGeeksHubs.component.tsx
@@ -14,22 +14,25 @@ export default function FlagGeeksHubsComponent() {
         return (
           <div key={`row-${row}`} className={`relative flex flex-row w-full flex-1`}>
             {columns.map((column) => {
+              const isRedBackground = (
+                ((+column > 10 && +column < 30) && (+row === 5)) ||
+                (+column === 11 && (+row > 5 && +row < 12)) ||
+                (+column === 29 && (+row > 5 && +row < 12)) ||
+                ((+column > 10 && +column < 19) && (+row === 12)) ||
+                ((+column > 22 && +column < 30) && (+row === 12)) ||
+                (+column === 19 && +row === 13) ||
+                (+column === 20 && +row === 14) ||
+                (+column === 21 && +row === 15) ||
+                (+column === 22 && (+row < 16 && +row > 11)) ||
+                (+row === 8 && (+column > 17 && +column < 23)) ||
+                (+row === 9 && (+column > 18 && +column < 22)) ||
+                (+row === 10 && +column === 20)
+              );
+              const backgroundClass = isRedBackground ? 'bg-red-500' : 'bg-slate-200';
               return (
                 <div
                   key={`row-${row}-column-${column}`}
-                  className={`h-full relative flex-1 bg-slate-200
-                  ${(+column > 10 && +column < 30) && (+row === 5) ? 'bg-red-500' : ''}
-                  ${+column === 11 && (+row > 5 && +row < 12) ? 'bg-red-500' : ''}
-                  ${+column === 29 && (+row > 5 && +row < 12) ? 'bg-red-500' : ''}
-                  ${(+column > 10 && +column < 19) && (+row === 12) ? 'bg-red-500' : ''}
-                  ${(+column > 22 && +column < 30) && (+row === 12) ? 'bg-red-500' : ''}
-                  ${(+column === 19 && +row === 13) ? 'bg-red-500' : ''}
-                  ${(+column === 20 && +row === 14) ? 'bg-red-500' : ''}
-                  ${(+column === 21 && +row === 15) ? 'bg-red-500' : ''}
-                  ${+column === 22 && (+row < 16 && +row > 11) ? 'bg-red-500' : ''}
-                  ${+row === 8 && (+column > 17 && +column < 23) ? 'bg-red-500' : ''}
-                  ${+row === 9 && (+column > 18 && +column < 22) ? 'bg-red-500' : ''}
-                  ${+row === 10 && +column === 20 ? 'bg-red-500' : ''}
+                  className={`h-full relative flex-1 ${backgroundClass}
                   ${row === '1' && column === '1' ? 'rounded-tl-3xl' : ''} ${row === rows.length.toString() && column === columns.length.toString() ? 'rounded-br-3xl' : ''} ${row === rows.length.toString() && column === '1' ? 'rounded-bl-3xl' : ''} ${row === '1' && column === columns.length.toString() ? 'rounded-tr-3xl' : ''}
                   `}
                   style={{
